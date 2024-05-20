21bbfa63fb32ffc6533131d5a6bca6446a5f67ac
Wonho Jo <tridath3@gmail.com>
2024-05-20 20:26:52 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
+
+fn handle_client(mut stream: std::net::TcpStream) {
+    println!("Accepted new connection");
+    loop {
+        let mut buf: [u8; 1024] = [0; 1024];
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                // Connection was closed by the client
+                println!("Client closed the connection");
+                break;
+            }
+            Ok(_) => {
+                // Echo back a PONG response
+                if let Err(e) = stream.write(b"+PONG\r\n") {
+                    println!("Failed to write to stream: {}", e);
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("Error reading from stream: {}", e);
+                break;
+            }
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,16 +35,12 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
-                println!("accepted new connection");
-                loop {
-                    let mut buf: [u8; 1024] = [0; 1024];
-                    s.read(&mut buf).unwrap();
-                    s.write(b"+PONG\r\n").unwrap();
-                }
+            Ok(s) => {
+                // Spawn a new thread to handle the connection
+                thread::spawn(move || handle_client(s));
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting connection: {}", e);
             }
         }
     }
