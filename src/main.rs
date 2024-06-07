f4afea8613be0dd3695cfd0240807023d2ca5a49
Wonho Jo <tridath3@gmail.com>
2024-06-07 15:17:45 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::thread;
+use anyhow::Result;
+use resp::Value;
+use tokio::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: std::net::TcpStream) {
-    println!("Accepted new connection");
+mod resp;
+
+#[tokio::main]
+
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
     loop {
-        let mut buf: [u8; 1024] = [0; 1024];
-        match stream.read(&mut buf) {
-            Ok(0) => {
-                // Connection was closed by the client
-                println!("Client closed the connection");
-                break;
-            }
-            Ok(_) => {
-                // Echo back a PONG response
-                if let Err(e) = stream.write(b"+PONG\r\n") {
-                    println!("Failed to write to stream: {}", e);
-                    break;
-                }
+        let stream = listener.accept().await;
+        match stream {
+            Ok((stream, _)) => {
+                println!("accepted new connection");
+                tokio::spawn(async move { handle_conn(stream).await });
             }
             Err(e) => {
-                println!("Error reading from stream: {}", e);
-                break;
+                println!("error: {}", e);
             }
         }
     }
 }
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+async fn handle_conn(stream: TcpStream) {
+    let mut handler = resp::RespHandler::new(stream);
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        let value = handler.read_value().await.unwrap();
+        // println!("value: {:?}", value);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(s) => {
-                // Spawn a new thread to handle the connection
-                thread::spawn(move || handle_client(s));
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
+                c => panic!("error occured while processing command: {}", c),
             }
-            Err(e) => {
-                println!("Error accepting connection: {}", e);
-            }
-        }
+        } else {
+            println!("closing connection");
+            break;
+        };
+
+        // println!("response: {:?}", response);
+        handler.write_value(response).await.unwrap();
+    }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
+    match value {
+        Value::Array(a) => Ok((
+            unpack_bulk_str(a.first().unwrap().clone())?,
+            a.into_iter().skip(1).collect(),
+        )),
+        _ => Err(anyhow::anyhow!("unknown command")),
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("not a bulk string")),
     }
 }
